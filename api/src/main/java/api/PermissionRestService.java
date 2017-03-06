94ba674f653623cb3b4c09f109ab52fac7c6e75b
Wulfric Wang <lufeihaidao+code@gmail.com>
2017-03-06 16:43:44 +0800

    consumer rest

--- api/src/main/java/api/PermissionRestService.java
+++ api/src/main/java/api/PermissionRestService.java
@@ -1,11 +1,20 @@
 package api;
 
+import com.alibaba.dubbo.rpc.protocol.rest.support.ContentType;
+
 import javax.validation.constraints.Min;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
 import java.util.List;
 
 /**
  * Created by haidao on 06/03/2017.
  */
+@Path("permissions")
+@Consumes({MediaType.APPLICATION_JSON})
+@Produces({ContentType.APPLICATION_JSON_UTF_8})
 public interface PermissionRestService {
-    List<String> getPermissions(@Min(value = 1L, message = "User ID must be greater than 1") Long id);
+    @GET
+    @Path("{id : \\d+}")
+    List<String> getPermissions(@PathParam("id") @Min(value = 1L, message = "User ID must be greater than 1") Long id);
 }
--- consumer/src/main/java/consumer/DemoConsumer.java
+++ consumer/src/main/java/consumer/DemoConsumer.java
@@ -1,5 +1,6 @@
 package consumer;
 
+import api.PermissionRestService;
 import api.PermissionService;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -8,10 +9,14 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  */
 public class DemoConsumer {
     public static void main(String[] args) {
-        //测试常规服务
+        // 测试常规服务
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("classpath*:META-INF/spring/*.xml");
         context.start();
+        // dubbo 协议
         PermissionService permissionService = context.getBean(PermissionService.class);
         System.out.println(permissionService.getPermissions(1L));
+        // rest 规范
+        PermissionRestService permissionRestService = context.getBean(PermissionRestService.class);
+        System.out.println(permissionRestService.getPermissions(2L));
     }
 }
